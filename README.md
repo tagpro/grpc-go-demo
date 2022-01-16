c38dc0a8784d6b3cd0756068420cde407cc24bb3
Jaspreet Singh <findjsk@gmail.com>
2022-01-16 19:42:35 +1100

    Adding math client

--- README.md
+++ README.md
@@ -44,3 +44,15 @@
 
     - `grpc_cli ls localhost:8000 -l` lists the services available on the server.
     - `grpc_cli call localhost:8000 Add "num1: 1, num2: 2"` calls the Add service on the server.
+
+### Calling the client
+
+    - `go run ./client.go -num1=3 -num2=4` calls the Add service on the server.
+
+
+### Resources
+
+- [Protobuf](https://developers.google.com/protocol-buffers/docs/overview)
+- [Go Proto plugins](https://grpc.io/docs/languages/go/quickstart/#prerequisites)
+- [grpc_cli](https://github.com/grpc/grpc/blob/master/doc/command_line_tool.md)
+- [grpc tutorial](https://grpc.io/docs/tutorials/basic/)
--- client.go
+++ client.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"log"
+
+	pb "github.com/tagpro/grpc-go-demo/proto"
+	"google.golang.org/grpc"
+)
+
+var (
+	server     = "localhost:8000"
+	num1, num2 int64
+)
+
 func main() {
 
+	flag.Int64Var(&num1, "num1", 0, "first number")
+	flag.Int64Var(&num2, "num2", 0, "second number")
+	flag.Parse()
+
+	// Set up a connection to the server.
+	conn, err := grpc.Dial("localhost:8000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	// Create a client
+	client := pb.NewMathServiceClient(conn)
+	response, err := client.Add(context.Background(), &pb.MathRequest{Num1: num1, Num2: num2})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("%d + %d = %d", num1, num2, response.Value)
 }
